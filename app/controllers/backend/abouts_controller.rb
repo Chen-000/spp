6d72a5e2afdeb1ea88c31a634ad3a63231019ce3
zwh9626 <974226958@qq.com>
2016-03-17 17:56:53 +0800

    代码规范

--- app/controllers/backend/abouts_controller.rb
+++ app/controllers/backend/abouts_controller.rb
@@ -4,29 +4,25 @@ class Backend::AboutsController < Backend::BaseController
         
   def index 
     @about = About.find(params[:id]) if About.pluck(:id).include?(params[:id].to_i)
-    @about = About.first if !About.all.blank? && params[:id].blank?
-   
+    @about = About.first if !About.all.blank? && params[:id].blank? 
   end
 
   def create
     @about= About.new(about_params)
-      if @about.save
-    redirect_to backend_abouts_path
-      end
+    @about.save
+    redirect_to backend_abouts_path   
   end
 
   def update
     @about = About.find(params[:id]) if params[:id]
     @about.update_attributes(about_params) 
-            flash[:success] = '操作成功.'
+    flash[:success] = '操作成功.'
     redirect_to  backend_abouts_path(id: params[:id])
   end
 
   private			
     def about_params
       params.require(:about).permit(:item, :content, :id)
-  end
-
-
+    end
 
 end
