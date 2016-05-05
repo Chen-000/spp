a25214293b28161ab666416c415e4b828ceaa41f
chenyulin123 <2535148345@qq.com>
2016-05-05 09:12:36 +0800

    spp页面调整

--- app/controllers/backend/abouts_controller.rb
+++ app/controllers/backend/abouts_controller.rb
@@ -1,7 +1,5 @@
 class Backend::AboutsController < Backend::BaseController
-  
-  #before_filter :create
-        
+          
   def index 
     @about = About.find(params[:id]) if About.pluck(:id).include?(params[:id].to_i)
     @about = About.first if !About.all.blank? && params[:id].blank? 
@@ -39,6 +37,12 @@ class Backend::AboutsController < Backend::BaseController
     @about= About.new(about_params)
     @about.save
     redirect_to backend_abouts_path   
+  end
+    def update
+    @about = About.find(params[:id]) if params[:id]
+    @about.update_attributes(about_params) 
+    flash[:success] = '操作成功.'
+    redirect_to  backend_abouts_path(id: params[:id])
   end
 
   def create_culture
